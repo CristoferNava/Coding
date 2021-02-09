c65594ef78477d87bcadb3e6693072a4fcdd4fef
CristoferNava <cj.carmonanava@ugto.mx>
2021-02-09 11:35:31 -0600

    palindrome_linked_list was added

---
+++ cristofer/palindrome_linked_list.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        slow = reverse(slow);
+        fast = head;
+        
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        return true;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
+
+// time: O(n) tenemos que recorrer todos los nodos de la lista
+// space: O(1) en el método reverse simplemente estamos cambiando la lista
+//       no estamos creando una nueva, por lo que no estamos usando espacio extra.

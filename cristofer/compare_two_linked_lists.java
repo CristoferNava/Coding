81a159d527d82d0eecb91a0575ec6039227f8786
CristoferNava <cj.carmonanava@ugto.mx>
2021-02-10 14:06:44 -0600

    compare_two_linked_lists was added

---
+++ cristofer/compare_two_linked_lists.java
@@ -0,0 +1,22 @@
+/*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+    while (head1 != null || head2 != null) {
+        if (head1 == null || head2 == null) return false;
+        if (head1.data != head2.data) return false;
+        head1 = head1.next;
+        head2 = head2.next;
+    }
+    
+    return true;
+}
+
+// time: O(n) siendo n la lista más pequeña
+// space: (1) no creamos espacio extra en memoria

8ec5479bca3bd7f65d0b190c32eeb45930e727ea
CristoferNava <cj.carmonanava@ugto.mx>
2021-02-18 17:11:40 -0600

    height of a binary tree was added

--- cristofer/height_binary_tree.java
+++ cristofer/height_binary_tree.java
@@ -2,3 +2,7 @@ public static int height(Node root) {
     if (root == null) return -1;
     return 1 + Math.max(height(root.left), height(root.right));
 }
+
+// Time: O(n) since we have to traverse all the nodes
+// Space: O(h) being h the eight of the three since the maximun number of calls
+//        int the stack is the largest path, that is the eight.

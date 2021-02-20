1523136031bd60791c829993417d693707d89814
CristoferNava <cj.carmonanava@ugto.mx>
2021-02-20 14:02:16 -0600

    First version of BFS was added

---
+++ cristofer/BFS_Traversal.java
@@ -0,0 +1,43 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BFS_Traversal {
+    public static void main(String[] args) {
+        //      9
+        //  4       20
+        // 1  6   15  170
+        var root = new Node(9);
+        root.left = new Node(4);
+        root.right = new Node(20);
+        root.left.left = new Node(1);
+        root.left.right = new Node(6);
+        root.right.left = new Node(15);
+        root.right.right = new Node(170);
+
+        search(root);
+    }
+
+    public static void search(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            Node r = queue.poll();
+            System.out.printf("%d ", r.data);
+            if (r.left != null) queue.offer(r.left);
+            if (r.right != null) queue.offer(r.right);
+        }
+    }
+}
